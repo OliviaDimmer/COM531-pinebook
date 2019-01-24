a24d38553ca074a5caf406f75a15a91b0e5c6d89
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-01-23 19:53:46 -0600

    ch3

--- calc.rb
+++ calc.rb
@@ -1,2 +1,32 @@
 #! /usr/bin/env ruby
-puts 1 + 2
+puts 3 + 6
+puts 3 * 4
+puts 5-8
+puts 12/3
+
+# Output
+# irb(main):002:0> puts 3 + 6
+# 9
+# => nil
+# irb(main):003:0> puts 3 * 4
+# 12
+# => nil
+# irb(main):004:0> puts 5-8
+# -3
+# => nil
+# irb(main):005:0> puts 12/3
+# 4
+# => nil
+# irb(main):006:0>
+
+puts 365*24
+puts "I dont like" + "math"
+
+# Output
+# irb(main):007:0> puts 365*24
+# 8760
+# => nil
+# irb(main):008:0>
+# I dont likemath
+# => nil
+# irb(main):003:0> 
---
+++ string.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# OUTPUT:
+#
+#Hello, world!
+#(empty)
+#Good-bye!
+
+puts 'wow'*4
+puts 'You\'re swell!'
+
+# wowwowwowwow
+# => nil
+# irb(main):002:0>
+# You're swell!
+# => nil
+# irb(main):002:0>
